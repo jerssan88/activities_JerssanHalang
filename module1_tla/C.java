ffae9f4b75c8618e246db85e9914e497b4139f69
jerssan88 <72538909+jerssan88@users.noreply.github.com>
2020-11-19 12:55:03 +0800

    Add files via upload

---
+++ module1_tla/C.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itcpr.m1.pkg1;
+
+/**
+ *
+ * @author Acer
+ */
+
+
+    /**
+  
+     */public class Sample_String{
+  public static void main(String[] args){//Character at position
+String str_Sample = "RockStar";
+System.out.println("Character at position 5: " + str_Sample.charAt(5));
+
+System.out.println("Index of character 'S': " + str_Sample.indexOf('S'));}}
+
